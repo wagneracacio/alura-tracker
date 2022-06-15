6442da7245b05dc6e250cdd60d1462fbc8292974
Wagner <wmaluk@gmail.com>
2022-06-15 18:35:46 -0300

    changed routes to add and edit projects

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import Formulario from '../views/Projetos/Formulario.vue'
+
 const rotas: RouteRecordRaw[] = [{
     path: '/',
     name: 'Tarefas',
@@ -9,6 +11,15 @@ const rotas: RouteRecordRaw[] = [{
     path: '/projetos',
     name: 'Projetos',
     component: Projetos
+}, {
+    path: '/projetos/novo',
+    name: 'Novo Projeto',
+    component: Formulario
+}, {
+    path: '/projetos/:id',
+    name: 'Editar Projeto',
+    component: Formulario,
+    props: true
 }]
 
 
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -3,30 +3,31 @@
     <h1 class="title">
         Projetos
     </h1>
-    <form @submit.prevent="salvar">
-        <div class="field">
-            <label for="nomeDoProjeto" class="label">
-                Nome do Projeto
-            </label>
-            <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto">
-        </div>
-        <div class="field">
-            <button class="button" type="submit">
-                Salvar
-            </button>
-        </div>
-    </form>
+    <router-link to="/projetos/novo" class="button">
+        <span class="icon is-small">
+            <i class="fas fa-plus"></i>
+        </span>
+        <span>Novo projeto</span>
+    </router-link>
     <table class="table is-fullwidth">
         <thead>
             <tr>
                 <th>ID</th>
-                <th>NOME</th>
+                <th>Nome</th>
+                <th>Ações</th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="projeto in projetos" :key="projeto.id">
-                <td>{{projeto.id}}}</td>
+                <td>{{projeto.id}}</td>
                 <td>{{projeto.nome}}</td>
+                <td>
+                    <router-link :to="`/projetos/${projeto.id}`" class="button">
+                        <span class="icon is-small">
+                            <i class="fas fa-pencil-alt"></i>
+                        </span>
+                    </router-link>
+                </td>
             </tr>
         </tbody>
     </table>
@@ -41,21 +42,9 @@ import {
 } from 'vue'
 export default defineComponent({
     name: "Projetos",
-    data() {
-        return {
-            nomeDoProjeto: ''
-        }
-    },
-    methods: {
-        salvar() {
-            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-            this.nomeDoProjeto = ''
-        }
-    },
     setup () {
         const store = useStore()
         return {
-            store,
             projetos: computed(() => store.state.projetos)
         }
     }
