e108952c94b282cdbdf7a382661ee309a55054a5
Wagner <wmaluk@gmail.com>
2022-06-15 18:00:14 -0300

    add store configuration

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,54 +1,77 @@
 <template>
-  <div class="box formulario">
+<div class="box formulario">
     <div class="columns">
-      <div
-        class="column is-8"
-        role="form"
-        ari-label="Formulario para criação de uma nova tarefa"
-      >
-        <input
-          type="text"
-          class="input"
-          placeholder="Qual tarefa voce deseja iniciar?"
-          v-model="descricao"
-        />
-      </div>
-      <div class="column">
-        <Temporizador @aoTemporizadorFInalizado="finalizarTarefa"/>
-      </div>
+        <div class="column is-5" role="form" ari-label="Formulario para criação de uma nova tarefa">
+            <input type="text" class="input" placeholder="Qual tarefa voce deseja iniciar?" v-model="descricao" />
+        </div>
+        <div class="column is-3">
+            <div class="select">
+                <select v-model="idProjeto">
+                    <option value="">Selecione o projeto</option>
+                    <option 
+                      :value="projeto.id" 
+                      v-for="projeto in projetos" 
+                      :key="projeto.id"
+                    >
+                        {{projeto.nome}}
+                    </option>
+                </select>
+            </div>
+        </div>
+        <div class="column">
+            <Temporizador @aoTemporizadorFInalizado="finalizarTarefa" />
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import {
+    key
+} from "@/store";
+import {
+    computed,
+    defineComponent
+} from "vue"
+import {
+    useStore
+} from "vuex";
 import Temporizador from './Temporizador.vue'
 
 export default defineComponent({
-  name: "Formulário",
-  emits: ['aoSalvarTarefa'],
-  components: {
-    Temporizador
-  },
-  data () {
-    return {
-      descricao: ''
+    name: "Formulário",
+    emits: ['aoSalvarTarefa'],
+    components: {
+        Temporizador
+    },
+    data() {
+        return {
+            descricao: '',
+            idProjeto: ''
+        }
+    },
+    methods: {
+        finalizarTarefa(tempoDecorrido: number): void {
+            this.$emit('aoSalvarTarefa', {
+                duracaoEmSegundos: tempoDecorrido,
+                descricao: this.descricao,
+                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
+            })
+            this.descricao = ''
+        }
+    },
+    setup() {
+        const store = useStore(key)
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
     }
-  },
-  methods: {
-    finalizarTarefa (tempoDecorrido: number): void {
-      this.$emit('aoSalvarTarefa', {
-        duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao
-      })
-      this.descricao = ''
-    }
-  }
 });
 </script>
+
 <style>
 .formulario {
-  color: var(--texto-primario);
-  background-color: var(--bg-primario);
+    color: var(--texto-primario);
+    background-color: var(--bg-primario);
 }
 </style>
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -34,26 +34,29 @@
 </template>
 
 <script lang="ts">
+import { useStore } from '@/store'
 import {
+  computed,
     defineComponent
 } from 'vue'
-import IProjeto from '../interfaces/IProjeto'
 export default defineComponent({
     name: "Projetos",
     data() {
         return {
-            nomeDoProjeto: '',
-            projetos: [] as IProjeto[]
+            nomeDoProjeto: ''
         }
     },
     methods: {
         salvar() {
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto)
+            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
             this.nomeDoProjeto = ''
+        }
+    },
+    setup () {
+        const store = useStore()
+        return {
+            store,
+            projetos: computed(() => store.state.projetos)
         }
     }
 })
