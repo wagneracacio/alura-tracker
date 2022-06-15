fe9e2e17c91c6645172333194770ab56e78d3370
Wagner <wmaluk@gmail.com>
2022-06-15 19:11:21 -0300

    add enums in mutation and remove project

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -18,6 +18,7 @@
 
 <script lang="ts">
 import { useStore } from '@/store'
+import { ADICIONA_PROJETO, ALTERA_PROJETO } from '@/store/tipo-mutacoes'
 import {
       defineComponent
 } from 'vue'
@@ -42,12 +43,12 @@ export default defineComponent({
     methods: {
         salvar() {
             if (this.id) {
-                this.store.commit('ALTERA_PROJETO', {
+                this.store.commit(ALTERA_PROJETO, {
                     id: this.id,
                     nome: this.nomeDoProjeto
                 })
             } else {
-                this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = ''
             this.$router.push('/projetos')
--- src/views/Projetos/Lista.vue
+++ src/views/Projetos/Lista.vue
@@ -24,6 +24,11 @@
                             <i class="fas fa-pencil-alt"></i>
                         </span>
                     </router-link>
+                    <button class="button ml-2 is-danger" @click="excluir(projeto.id)">
+                        <spa class="icon is-small">
+                            <i class="fas fa-trash"></i>
+                        </spa>
+                    </button>
                 </td>
             </tr>
         </tbody>
@@ -33,16 +38,23 @@
 
 <script lang="ts">
 import { useStore } from '@/store'
+import { EXCLUIR_PROJETO } from '@/store/tipo-mutacoes'
 import {
   computed,
     defineComponent
 } from 'vue'
 export default defineComponent({
     name: "Lista",
+    methods: {
+        excluir (id: string) {
+            this.store.commit(EXCLUIR_PROJETO, id)
+        }
+    },
     setup () {
         const store = useStore()
         return {
-            projetos: computed(() => store.state.projetos)
+            projetos: computed(() => store.state.projetos),
+            store
         }
     }
 })
