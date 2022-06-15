a80745e955793c5f5f49034ad75caa570d897a2e
Wagner <wmaluk@gmail.com>
2022-06-15 18:51:20 -0300

    add sub routes

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -2,24 +2,28 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
 import Formulario from '../views/Projetos/Formulario.vue'
-
+import Lista from '../views/Projetos/Lista.vue'
 const rotas: RouteRecordRaw[] = [{
     path: '/',
     name: 'Tarefas',
     component: Tarefas
 }, {
     path: '/projetos',
-    name: 'Projetos',
-    component: Projetos
-}, {
-    path: '/projetos/novo',
-    name: 'Novo Projeto',
-    component: Formulario
-}, {
-    path: '/projetos/:id',
-    name: 'Editar Projeto',
-    component: Formulario,
-    props: true
+    component: Projetos,
+    children: [{
+        path: '',
+        name: 'Projetos',
+        component: Lista
+    }, {
+        path: 'novo',
+        name: 'Novo Projeto',
+        component: Formulario
+    }, {
+        path: ':id',
+        name: 'Editar Projeto',
+        component: Formulario,
+        props: true
+    }]
 }]
 
 
