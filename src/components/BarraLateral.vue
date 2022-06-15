8fa57f3d2321dcbab82b1943ff84e472af294b12
Wagner <wmaluk@gmail.com>
2022-06-15 16:59:58 -0300

    add router navbar

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -6,6 +6,22 @@
         <button class="button" @click="alterarTema">
             {{textoBotao}}
         </button>
+        <nav class="panel mt-5">
+            <ul>
+              <li>
+                <router-link to="/" class="link">
+                    <i class="fas fa-tasks"></i>
+                    tarefas
+                </router-link>
+              </li>  
+              <li>
+                <router-link to="/projetos" class="link">
+                    <i class="fas fa-diagram"></i>
+                    projetos
+                </router-link>
+              </li>  
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -49,5 +65,17 @@ header {
         padding: 2.5rem;
         height: auto;
     }
+}
+.panel li {
+    margin: 8px 0;
+}
+.link {
+    color: #fff;
+}
+.link:hover {
+    color: #FAF0CA;
+}
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,10 +1,14 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
-
+import Projetos from '../views/Projetos.vue'
 const rotas: RouteRecordRaw[] = [{
     path: '/',
     name: 'Tarefas',
     component: Tarefas
+}, {
+    path: '/projetos',
+    name: 'Projetos',
+    component: Projetos
 }]
 
 
