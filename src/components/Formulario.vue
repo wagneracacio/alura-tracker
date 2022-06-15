8de1ac6550f1ba95e5c0e0090159936f752d049f
Wagner <wmaluk@gmail.com>
2022-06-14 21:26:53 -0300

    add project result of first course

---
+++ src/components/Formulario.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="box formulario">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        ari-label="Formulario para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa voce deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFInalizado="finalizarTarefa"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import Temporizador from './Temporizador.vue'
+
+export default defineComponent({
+  name: "Formulário",
+  emits: ['aoSalvarTarefa'],
+  components: {
+    Temporizador
+  },
+  data () {
+    return {
+      descricao: ''
+    }
+  },
+  methods: {
+    finalizarTarefa (tempoDecorrido: number): void {
+      this.$emit('aoSalvarTarefa', {
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: this.descricao
+      })
+      this.descricao = ''
+    }
+  }
+});
+</script>
+<style>
+.formulario {
+  color: var(--texto-primario);
+  background-color: var(--bg-primario);
+}
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,31 @@
+<template >
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{tarefa.descricao || 'Tarefa sem descrição'}}
+            </div>
+            <div class="column">
+                <Cronomentro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+            </div>
+        </div>
+    </Box>
+</template>
+<script lang="ts">
+import ITarefa from '../interfaces/ITarefa'
+import { defineComponent, PropType } from 'vue'
+import Cronomentro from './Cronomentro.vue'
+import Box from './Box.vue'
+export default defineComponent({
+    name: 'Tarefa',
+    components: {
+        Cronomentro,
+        Box
+    },
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true
+        }
+    }
+})
+</script>
