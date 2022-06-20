1cc4f42f47df64dca691e0a422308ee8738753e0
Wagner <wmaluk@gmail.com>
2022-06-20 11:59:57 -0300

    add integration with api

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -17,14 +17,21 @@
 </template>
 
 <script lang="ts">
-import { TipoNotificacao } from '@/interfaces/INotificacao'
-import { notificacaoMixin } from '@/mixins/notificar'
-import { useStore } from '@/store'
-import useNotificador  from '@/hooks/notificador'
-import { ADICIONA_PROJETO, ALTERA_PROJETO } from '@/store/tipo-mutacoes'
 import {
-      defineComponent
+    TipoNotificacao
+} from '@/interfaces/INotificacao'
+//import { notificacaoMixin } from '@/mixins/notificar'
+import {
+    useStore
+} from '@/store'
+import useNotificador from '@/hooks/notificador'
+import {
+    defineComponent
 } from 'vue'
+import {
+    ALTERAR_PROJETO,
+    CADASTRAR_PROJETO
+} from '@/store/tipo-acoes'
 export default defineComponent({
     name: "Formulario",
     props: {
@@ -33,8 +40,8 @@ export default defineComponent({
         }
     },
     //mixins: [notificacaoMixin],
-    mounted () {
-        if(this.id) {
+    mounted() {
+        if (this.id) {
             const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
             this.nomeDoProjeto = projeto?.nome || ''
         }
@@ -47,22 +54,26 @@ export default defineComponent({
     methods: {
         salvar() {
             if (this.id) {
-                this.store.commit(ALTERA_PROJETO, {
+                this.store.dispatch(ALTERAR_PROJETO, {
                     id: this.id,
                     nome: this.nomeDoProjeto
-                })
+                }).then(() => this.lidarComSucesso())
             } else {
-                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
+                this.store.dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto)
+                    .then(() => this.lidarComSucesso())
             }
+        },
+        lidarComSucesso() {
             this.nomeDoProjeto = ''
             this.notificar(TipoNotificacao.SUCESSO, 'Excelente', 'O projeto foi cadastrado com successo')
             this.$router.push('/projetos')
-        },
-        
+        }
     },
-    setup () {
+    setup() {
         const store = useStore()
-        const { notificar } = useNotificador()
+        const {
+            notificar
+        } = useNotificador()
         return {
             store,
             notificar
--- src/views/Projetos/Lista.vue
+++ src/views/Projetos/Lista.vue
@@ -38,6 +38,7 @@
 
 <script lang="ts">
 import { useStore } from '@/store'
+import { OBTER_PROJETOS, REMOVER_PROJETO } from '@/store/tipo-acoes'
 import { EXCLUIR_PROJETO } from '@/store/tipo-mutacoes'
 import {
   computed,
@@ -47,11 +48,12 @@ export default defineComponent({
     name: "Lista",
     methods: {
         excluir (id: string) {
-            this.store.commit(EXCLUIR_PROJETO, id)
+            this.store.dispatch(REMOVER_PROJETO, id)
         }
     },
     setup () {
         const store = useStore()
+        store.dispatch(OBTER_PROJETOS)
         return {
             projetos: computed(() => store.state.projetos),
             store
